7307c75de77cd81e4a8f4263abcffd4167db1f62
Omar Abdiwali <omarabdiwali17@gmail.com>
2025-03-21 14:03:27 -0600

    removed unused code

--- pages/[code].js
+++ pages/[code].js
@@ -1,24 +1,3 @@
-// import { useRouter } from "next/router";
-// import { useEffect } from "react";
-
-// export default function Home() {
-//   const router = useRouter();
-//   useEffect(() => {
-//     if (!router.isReady) return;
-//     let code = router.query.code
-
-//     fetch("/api/get", {
-//       method: "POST",
-//       body: JSON.stringify({ code })
-//     }).then(res => res.json()).then(data => router.replace(data.url))
-//       .catch(err => console.error(err));
-
-//   }, [router.isReady])
-
-//   return (
-//     <div>Redirecting...</div>
-//   )
-// }
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
